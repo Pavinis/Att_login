5497664b357f570950697d1acf20cab5bf6a108a
Paulo V S <paulo_v_siqueira@estudante.sesisenai.org.br>
2024-04-17 12:47:27 -0300

    commit inicial

---
+++ script/script.js
@@ -0,0 +1,8 @@
+let usuario = [[]];
+
+function logarConta() {
+    let nome = document.getElementById("nome").value;
+    let senha = document.getElementById("senha").value;
+    console.log(nome);
+    console.log(senha);
+}
