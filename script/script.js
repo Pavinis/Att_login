a62fe3e2ccc53093c65fdc6d4e6030ed5fe96b4c
Paulo V S <paulo_v_siqueira@estudante.sesisenai.org.br>
2024-04-19 12:50:14 -0300

    css

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 //estou setando os nomes e senhas possíveis para entrar.
-let usuarioNome = ["paulo","rodosvaldo","teca jacu"]
+let usuarioNome = ["paulo","rodosvaldo","teca jacu"];
 let usuarioSenha = ["1234","rodavira","mazza"];
 
 //verifica se o usuario e senha esta correto, se sim redireciona, senão alerta.
@@ -13,12 +13,23 @@ function logarConta() {
         if (usuIndex === -1) {
             alert("Usuário incorreto.\nDesculpe! bah! tchê!");
         } else {
-            alert("Senha incorreta.\nDesculpe! bah! tchê!");
+            alert("Senha incorreta.\nDesculpe! ma que bah!... tchê!");
         }
     }
+}
 
-    console.log(nome);
-    console.log(senha);
-    console.log(usuIndex);
+//Diz que usuario acessou e compoe a lista de usuarios existentes
+function outra(){
+    let i = 0;
+    let lista = "";
 
+    for(i = 0; i < usuarioNome.length; i + 1) {
+        lista = (lista + usuarioNome[i]);
+        lista = (lista + " ");
+        lista = (lista + usuarioSenha[i]);
+        lista = (lista + ", ");
+    }
+
+    document.getElementById('lista').innerHTML = (lista);
+    console.log(lista);
 }
